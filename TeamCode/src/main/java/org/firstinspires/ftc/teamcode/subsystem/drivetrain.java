48946dd99d29db2f364a5fd33bec184fb4d1c227
Unknown <xthefast4x@gmail.com>
2018-03-19 15:58:42 -0700

    teleop fin

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/drivetrain.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.subsystem;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by JUSTIN on 3/15/18.
+ */
+
+public class drivetrain {
+    public DcMotor right_top;
+    public DcMotor right_bottom;
+    public DcMotor left_bottom;
+    public DcMotor left_top;
+    public drivetrain(DcMotor right_Top, DcMotor right_Bottom, DcMotor left_Bottom, DcMotor left_Top) {
+        this.right_bottom = right_Bottom;
+        this.right_top = right_Top;
+        this.left_bottom = left_Bottom;
+        this.left_top = left_Top;
+    }
+    public void mechanumDrive(double movX, double movY, double rotation) {
+
+        //assign motor powers their inputs based on position on the robot
+        double forwardLeftPower = Range.clip((-1*movX - movY), -1, 1) - rotation;
+        double forwardRightPower = -1*(Range.clip((-1*movX + movY), -1, 1) - rotation);
+        double backLeftPower = Range.clip((movX - movY), -1, 1) - rotation;
+        double backRightPower = -1*(Range.clip((movX + movY), -1, 1) - rotation);
+
+        left_bottom.setPower(forwardLeftPower);
+        right_top.setPower(forwardRightPower);
+        left_bottom.setPower(backLeftPower);
+        right_bottom.setPower(backRightPower);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele_op.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.media.MediaPlayer;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystem.drivetrain;
+
+import static android.R.attr.name;
+
+/**
+ * Created by JOSEPH STALIN on 3/19/18.
+ */
+@TeleOp(name= "KV-1_TELEOP", group= "communism")
+public class tele_op extends OpMode{
+    public drivetrain robot;
+    public static final int TICKS_PER_ROTATION = 1440;
+    public static final int WHEEL_DIAMETER     = 4   ;
+    public double           rotations                ;
+    public double           position                 ;
+    public MediaPlayer      DJ_Stalin_YEET           ;
+
+
+    @Override
+    public void init() {
+         robot = new drivetrain(hardwareMap.dcMotor.get("right_top"), hardwareMap.dcMotor.get("right_bottom"), hardwareMap.dcMotor.get("left_bottom"), hardwareMap.dcMotor.get("left_top"));
+    }
+    @Override
+    public void start() {
+
+    }
+    @Override
+    public void loop() {
+        if(gamepad1.left_trigger > 0) {
+            robot.mechanumDrive(-Math.pow(gamepad1.left_stick_x, 3), -Math.pow(gamepad1.left_stick_y, 3), Math.pow(gamepad1.right_stick_x, 3));
+        }
+        else {
+            robot.mechanumDrive(-Math.pow(gamepad1.left_stick_x, 9), -Math.pow(gamepad1.left_stick_y, 9), Math.pow(gamepad1.right_stick_x, 9));
+        }
+
+
+
+    }
+}
